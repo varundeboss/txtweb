74a116689d811b950e3430c4a6b8f31e7ae648e1
varunthyboss@gmail.com <varunthyboss@gmail.com@2e7ea6d5-f8f9-52c5-0646-323eab8787ed>
2012-10-18 17:00:44 +0000

    Added personal details adding capability

--- Apps/PersonalDetails/personal_config.py
+++ Apps/PersonalDetails/personal_config.py
@@ -1,4 +1,6 @@
-AUTH_CODE = lambda code:code
+from math import sin
+
+AUTH_CODE = lambda code : True if code and str(code).isdigit() and str(code)[0] + str(code)[1:int(str(code)[0] or 0)+1] + str(sin(int(str(code)[1:int(str(code)[0])+1] or 0))).split('.')[1][:4] == str(code) and int(str(code)[1:int(str(code)[0])+1] or 0) != 0 else False
 
 PERSONAL_DICT = {
                     "CHNHADDR" : "R.Varun Kumar, Ground Floor plot no 20, 3rd street, Kumaraguru Avenue, Metukuppam. Near ASV SunTechPark. Chennai-600097. Mobile-8870435477.",
@@ -20,6 +22,12 @@ PERSONAL_DICT = {
 
 ERR_MSGS = { 
             "AUTH" : "The auth code is invalid. Please try again with a valid auth code.",
-            "KEYERR" : "The keyword is invalid. Available keywords are : "+str(PERSONAL_DICT.keys()),
-            "WELCOME" : "Welcome Varun. For getting your personal details send '@varun prl <keyword> <authcode>'",
+            "KEYERR" : "The keyword is invalid. Send @varundeboss <code> help for existing keywords",
+            "WELCOME" : "Welcome Varun. For getting your personal details send '@varundeboss prl <authcode> <keyword>'",
+            "ADDERR" : "For adding a new personal detail send @varundeboss plr <code> <keyword--content>. Sending existing keyword updates existing content",
+            "GETERR" : "For getting your personal detail send @varundeboss plr <code> <keyword>",
+            "HELP" : "Existing Keywords : "+','.join(PERSONAL_DICT.keys()),
+            "ADDSUC" : "Your personal details has been added successfully",
+            "ADDFAIL" : "Failed to add your personal detail. Please try again later",
+            "UPSUC" : "Your personal details has been updated successfully",
            }                    
