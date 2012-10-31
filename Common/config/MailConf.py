cffbba73ab03bf59d1c257efdcb549f2405974bd
varunthyboss@gmail.com <varunthyboss@gmail.com@2e7ea6d5-f8f9-52c5-0646-323eab8787ed>
2012-10-31 16:14:02 +0000

    Adding new mailconf for sending mails

--- Common/config/MailConf.py
+++ Common/config/MailConf.py
@@ -1,4 +1,1 @@
-#mail_config = {'server': 'smtp.googlemail.com', 'port':465, 'startSSL':True, 'username': 'varunthyboss@gmail.com', 'password':'VA@rundebo0s'}
-
-mail_config = {'server': 'smtps.global-analytics.com', 'port':465, 'startSSL':True, 'username': 'varun.r@global-analytics.com', 'password':'godisnowhere143*'}
-
+mail_config = {'server': 'smtp.gmail.com', 'port':465, 'startSSL':True, 'username': 'aioa.txtweb@gmail.com', 'password':'VA@rundebo)s'}
