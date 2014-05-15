069a472df8ccaa0ad9cd2c7edd18af9c8e9ab46d
varunthyboss@gmail.com <varunthyboss@gmail.com@2e7ea6d5-f8f9-52c5-0646-323eab8787ed>
2014-05-15 06:08:46 +0000

    Removing stuffs

--- Apps/PersonalDetails/personal_config.py
+++ Apps/PersonalDetails/personal_config.py
@@ -8,8 +8,6 @@ FILE_NAME_MAP = {
                 }
 
 PERSONAL_DICT = {
-                    "CHNHADDR" : "R.Varun Kumar, Ground Floor plot no 20, 3rd street, Kumaraguru Avenue, Metukuppam. Near ASV SunTechPark. Chennai-600097. Mobile-8870435477.",
-                    "TVLHADDR" : "R.Varun Kumar, 216 Shenbagam pillai double street, Tirunelveli-Town. Pin-627006. Mobile-8870435477. LandLine-04622620619",
                     "GAADDR" : "GA",
                 }
 
